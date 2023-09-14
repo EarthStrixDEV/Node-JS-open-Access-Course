d4f9d898fa75dbaa4b90ce9679c3d21762ebd97b
EarthStrix <=>
2023-09-15 00:56:14 +0700

    product page

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
-const productRouter = express.Router()
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const products = require('./data/products.json')
+const productRouter = express.Router();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
 
-app.set('views' ,"./src/view")
-app.set('view engine' ,'ejs')
+app.set("views", "./src/view");
+app.set("view engine", "ejs");
 
-productRouter.route('/').get((request ,response) => {
-    response.send('Hello World ,I\'m products 0')
-})
+productRouter.route("/").get((request, response) => {
+  response.render("product", {
+    products
+  });
+});
 
-productRouter.route('/1').get((request ,response) => {
-    response.send('Hello World ,I\'m products 1')
-})
+productRouter.route("/1").get((request, response) => {
+  response.send("Hello World ,I'm products 1");
+});
 
-app.use('/products' ,productRouter)
+app.use("/products", productRouter);
 
-app.get('/', (request ,response) => {
-    response.render('index' ,{username: 'EarthStrix' ,customer: ['Earth','Pim','May']})
-})
+app.get("/", (request, response) => {
+  response.render("index");
+});
 
-app.listen(port ,() => {
-    console.log("listening on port " + port);   
-})
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
