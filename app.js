bb81cc40ec79cb968379ce397fa2626a3fe4cb62
EarthStrix <=>
2023-09-14 23:52:00 +0700

    new template

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get("/" ,(request ,response) => {
-    response.send("Hello Born To Dev Co. ,LTD" + new Date().toLocaleTimeString())
-})
+app.use(express.static(path.join(__dirname, './public')))
+
 
 app.listen(port ,() => {
-    console.log("listening on port " + port);
+    console.log("listening on port " + port);   
 })
